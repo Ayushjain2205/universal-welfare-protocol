246108c8eeeaa0d4604b23e152e79ce2999b26c8
Ayush Jain <ayushjain2205@gmail.com>
2023-10-21 17:17:51 +0530

    Sectionify IDverification

--- src/app/create/page.jsx
+++ src/app/create/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "../components/sections/About";
 import Nominee from "../components/sections/Nominee";
+import IDverification from "../components/sections/IDverification";
 
 const Create = () => {
   return (
     <>
       {/* <About /> */}
-      <Nominee />
+      {/* <Nominee /> */}
+      <IDverification />
     </>
   );
 };
